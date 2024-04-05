fd2ca0ee1e5320968446f82e5d6ef69e1c23d614
nki1121 <nkiuniv@gmail.com>
2024-04-05 21:27:17 +0900

    Initial Commit

---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by 中井晋平 on 2024/04/05.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
